24bd81218b5ebe3647d917cd92ba855a19c0ab1a
大塚宣孝 <yoshitaka@MacBook-Air.local>
2025-07-03 23:11:12 +0900

    Added name input and greeting

--- dice_game.cpp
+++ dice_game.cpp
@@ -1,16 +1,21 @@
-// dice_game.cpp
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 
 using namespace std;
 
 int main() {
+    string name;
+    cout << "Enter your name: ";
+    getline(cin, name);
+    cout << "Hello, " << name << "!" << endl;
+
     srand(time(nullptr));
 
     cout << "Rolling dice..." << endl;
-    int die1 = std::rand() % 6 + 1;
-    int die2 = std::rand() % 6 + 1;
+    int die1 = rand() % 6 + 1;
+    int die2 = rand() % 6 + 1;
 
     cout << "Die 1: " << die1 << endl;
     cout << "Die 2: " << die2 << endl;
